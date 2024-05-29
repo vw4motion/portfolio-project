959cbf4ccbc6b4a713f7b49beaf65922f7da6e12
handsonapibook <159751056+handsonapibook@users.noreply.github.com>
2024-05-28 21:33:15 -0500

    Add files via upload

--- chapter7/complete/sdk/tests/test_pyswc.py
+++ chapter7/complete/sdk/tests/test_pyswc.py
@@ -24,33 +24,33 @@ Typical usage example:
 config = SWCConfig(url="http://127.0.0.1:8000",backoff=False)
 client = SWCClient(config)    
 
-# def test_health_check():
-#     """Tests health check from SDK"""
-#     config = SWCConfig()
-#     client = SWCClient(config)
-#     response = client.get_health_check()
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "API health check successful"}
+def test_health_check():
+    """Tests health check from SDK"""
+    config = SWCConfig()
+    client = SWCClient(config)
+    response = client.get_health_check()
+    assert response.status_code == 200
+    assert response.json() == {"message": "API health check successful"}
 
-# def test_health_check_with_URL():
-#     """Tests health check from SDK"""
-#     config = SWCConfig(url="http://127.0.0.1:8000")
-#     client = SWCClient(config)    
-#     response = client.get_health_check()
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "API health check successful"}
+def test_health_check_with_URL():
+    """Tests health check from SDK"""
+    config = SWCConfig(url="http://127.0.0.1:8000")
+    client = SWCClient(config)    
+    response = client.get_health_check()
+    assert response.status_code == 200
+    assert response.json() == {"message": "API health check successful"}
 
-# def test_list_leagues():
-#     """Tests get leagues from SDK"""
-#     config = SWCConfig(url="http://127.0.0.1:8000")
-#     client = SWCClient(config)    
-#     leagues_response = client.list_leagues()
-#     # Assert the list is not empty
-#     assert isinstance(leagues_response, list)
-#     # Assert each item in the list is an instance of League
-#     for league in leagues_response:
-#         assert isinstance(league, League)
-#     assert len(leagues_response) == 5
+def test_list_leagues():
+    """Tests get leagues from SDK"""
+    config = SWCConfig(url="http://127.0.0.1:8000")
+    client = SWCClient(config)    
+    leagues_response = client.list_leagues()
+    # Assert the list is not empty
+    assert isinstance(leagues_response, list)
+    # Assert each item in the list is an instance of League
+    for league in leagues_response:
+        assert isinstance(league, League)
+    assert len(leagues_response) == 5
 
 def test_list_leagues_no_backoff():
     """Tests get leagues from SDK without backoff"""
